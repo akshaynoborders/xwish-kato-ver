d42ba9faad83594860a37c1de0323ee29ce3daa2
Akshay S <akshay@webllisto.com>
2020-01-06 14:45:18 +0530

    slider auto play

--- js/main.js
+++ js/main.js
@@ -39,6 +39,8 @@ jQuery('document').ready(function($){
 		slidesToScroll: 1,
 		dots: true,
 		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
 		prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left'></i></button>",
 		nextArrow:"<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'></i></button>",
 		responsive: [
